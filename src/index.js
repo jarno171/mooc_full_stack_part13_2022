e219990aa589adfe15f3b0afd1d3aa30e3e7ff70
Jarno <jarno.varis@gmail.com>
2023-01-19 18:08:46 +0200

    feature: refactored the app to different folders, and also added the put endpoint for manipulating likes

--- src/index.js
+++ src/index.js
@@ -1,73 +1,22 @@
-require('dotenv').config()
+const { PORT, DATABASE_URL } = require('./util/config')
+
 const { Sequelize, Model, DataTypes } = require('sequelize')
 const express = require('express')
 const app = express()
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  },
-});
+const { connectToDatabase } = require('./util/db')
 
-class Blog extends Model {}
-Blog.init({
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  author: {
-    type: DataTypes.TEXT
-  },
-  url: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  title: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  likes: {
-    type: DataTypes.INTEGER,
-    defaultValue: 0
-  }
-}, {
-  sequelize,
-  underscored: true,
-  timestamps: false,
-  modelName: 'blog'
-})
+const blogsRouter = require('./controllers/blog')
 
 app.use(express.json())
 
-app.get('/api/blogs', async (req, res) => {
-  const blogs = await Blog.findAll()
-  res.json(blogs)
-})
+app.use('/api/blogs', blogsRouter)
 
-app.post('/api/blogs', async (req, res) => {
-  try {
-    const blog = await Blog.create(req.body)
-    return res.json(blog)
-  } catch(error) {
-    return res.status(400).json({ error })
-  }
-})
+const start = async () => {
+  await connectToDatabase()
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+}
 
-app.delete('/api/blogs/:id', async (req, res) => {
-  try {
-    await Blog.destroy({ where: { id: req.params.id }})
-
-    response.status(204).end()
-  } catch(error) {
-    return res.status(404).json({ error })
-  }
-})
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+start()
