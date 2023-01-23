1d52030ec5483ae95907068a4ff820f25a74a8e9
Jarno <jarno.varis@gmail.com>
2023-01-23 20:47:32 +0200

    feature: added the table generation into migrations, with rollback, and it seems to work

--- src/models/index.js
+++ src/models/index.js
@@ -1,11 +1,6 @@
 const Blog = require('./blog')
 const User = require('./user')
 
-User.hasMany(Blog)
-Blog.belongsTo(User)
-Blog.sync({ alter: true })
-User.sync({ alter: true })
-
 module.exports = {
   Blog,
   User
