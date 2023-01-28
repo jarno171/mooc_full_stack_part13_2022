0d3ec415b593218e2e410fc31c914b9cbcd240aa
Jarno <jarno.varis@gmail.com>
2023-01-28 12:02:13 +0200

    feature: now readinglist can be toggled to read, and it works only for logged in users

--- src/controllers/readinglists.js
+++ src/controllers/readinglists.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 
-const { Readinglists } = require('../models')
+const { Readinglists, User } = require('../models')
+
+const { tokenExtractor } = require('../util/middleware')
 
 router.get('/', async (req, res) => {
   const Readinglists = await Readinglists.findAll({
@@ -13,6 +15,22 @@ router.post('/', async (req, res, next) => {
     const addeReadinglist = await Readinglists.create(req.body)
     res.json(addeReadinglist)
   } catch(error) {
+    next(error)
+  }
+})
+
+router.put('/:id', tokenExtractor, async (req, res) => {
+
+  const user = await User.findByPk(req.decodedToken.id)
+  const readinglist = await Readinglists.findByPk(req.params.id)
+
+  if (user && readinglist && user.id === readinglist.userId && req.body.read) {
+    readinglist.read = req.body.read
+    await readinglist.save()
+    res.json(readinglist)
+  } else {
+    const error = new Error('readinglist id is missing')
+    error.name = 'IdMissing'
     next(error)
   }
 })
